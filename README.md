29b9b1a5fd2cbdba0db245fa89f0058277a444c9
Anupam Tripathi <arvindtri.70@gmail.com>
2022-01-15 17:55:55 +0530

    All files are added.
    All files are added.

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# Project- Mini Project
+## Gaming Website
+
+This is mini project website based on gaming. And all images are taken from google.
+
+
+
+## Demo
+
+https://tri-anupam.github.io/gaming-website.github.io/
+
+
+## Authors
+
+- [@Anupam_tripathi](https://github.com/tri-anupam)
+- [@Himanshu_pandey](https://github.com/Himanshu-Panday)
+
---
+++ script.js
@@ -0,0 +1,30 @@
+var navlink = document.getElementById("nav-link");
+function showMenu() {
+    navlink.style.right = "0";
+}
+function hideMenu() {
+    navlink.style.right = "-200px";
+}
+var counter = 1;
+setInterval(function(){
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if(counter > 4){
+    counter = 1;
+  }
+}, 5000);
+
+
+
+// ------------login Form--------------
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
